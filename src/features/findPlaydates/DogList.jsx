0a2af8ea45b1012a1ac86a991977e23041f8592f
Kelly Lee <kellyws.lee@gmail.com>
2023-12-31 14:54:34 -0500

    Add sortby distance

--- src/features/findPlaydates/DogList.jsx
+++ src/features/findPlaydates/DogList.jsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { useAllDogs } from '../../hooks/useDogs';
+import { calDistance } from '../../utils/helpers';
 import MiniProfile from './MiniProfile';
 import Loader from '../../ui/Loader';
 
@@ -15,17 +16,32 @@ const DogList = () => {
   const searchQuery = searchParams.get('search') || '';
   const sizeFilters = searchParams.get('size')?.split(',') || [];
   const energyLevelFilters = searchParams.get('energyLevel')?.split(',') || [];
-  const sortBy = searchParams.get('sortBy');
-  const [field, direction] = sortBy.split('-');
+  const sortBy = searchParams.get('sortBy') || 'distance-asc';
+  const [field, direction] = sortBy ? sortBy.split('-') : [null, null];
   const modifier = direction === 'asc' ? 1 : -1;
 
   if (isLoadingAllDogs) return <Loader />;
-  if (errorAllDogs) {
+  if (errorAllDogs || !dogs) {
     toast.error('Error loading dogs');
     return null;
   }
 
-  const filteredDogs = dogs.filter((dog) => {
+  const userLat = searchParams.get('lat');
+  const userLng = searchParams.get('lng');
+  const originCoordinates =
+    userLat && userLng
+      ? [parseFloat(userLat), parseFloat(userLng)]
+      : [43.64, -79.4];
+  const dogsWithDistance = dogs
+    ? dogs.map((dog) => ({
+        ...dog,
+        distance: parseFloat(
+          calDistance(originCoordinates, [dog.lat, dog.lng]),
+        ).toFixed(1),
+      }))
+    : [];
+
+  const filteredDogs = dogsWithDistance.filter((dog) => {
     // Apply search filter
     if (
       searchQuery &&
@@ -56,13 +72,18 @@ const DogList = () => {
     return sizeMatch && energyLevelMatch;
   });
 
-  let sortedDogs = filteredDogs;
-  if (field === 'size')
-    sortedDogs = filteredDogs.sort((a, b) => (a[field] - b[field]) * modifier);
-  if (field === 'dateOfBirth')
-    sortedDogs = filteredDogs.sort(
-      (a, b) => (new Date(a[field]) - new Date(b[field])) * (modifier * -1),
+  // Sort dogs
+  let sortedDogs = [...filteredDogs];
+  if (field === 'distance') {
+    sortedDogs.sort((a, b) => (a.distance - b.distance) * modifier);
+  } else if (field === 'size') {
+    sortedDogs.sort((a, b) => (a.size - b.size) * modifier);
+  } else if (field === 'dateOfBirth') {
+    sortedDogs.sort(
+      (a, b) =>
+        (new Date(a.dateOfBirth) - new Date(b.dateOfBirth)) * modifier * -1,
     );
+  }
 
   return (
     <section className="flex h-44 w-full flex-col gap-2 overflow-auto md:h-96">
--- src/features/findPlaydates/MiniProfile.jsx
+++ src/features/findPlaydates/MiniProfile.jsx
@@ -1,43 +1,8 @@
 /* eslint-disable react/prop-types */
-import { Link, useSearchParams } from 'react-router-dom';
-// import { toast } from 'react-hot-toast';
-// import { Hearts } from 'react-loader-spinner';
-// import { useUser } from '../../hooks/useAuth';
-// import { useMyDog } from '../../hooks/useDogs';
-import { calculateAge, calDistance } from '../../utils/helpers';
+import { Link } from 'react-router-dom';
+import { calculateAge } from '../../utils/helpers';
 
 const MiniProfile = ({ dog }) => {
-  const [searchParams] = useSearchParams();
-  const userLat = searchParams.get('lat');
-  const userLng = searchParams.get('lng');
-  // const { user } = useUser();
-  // const { myDog, isLoading, error } = useMyDog(user?.id);
-
-  const DEFAULT_COORDINATES = [43.64, -79.4];
-
-  // if (isLoading)
-  //   return (
-  //     <Hearts
-  //       color="#ffbf69"
-  //       wrapperStyle={{
-  //         display: 'flex',
-  //         justifyContent: 'center',
-  //         alignItems: 'center',
-  //         height: '100vh',
-  //       }}
-  //     />
-  //   );
-  // if (error) return toast.error('Error fetching dog profile');
-
-  const originCoordinates =
-    userLat && userLng
-      ? [parseFloat(userLat), parseFloat(userLng)]
-      : DEFAULT_COORDINATES;
-
-  const distance = parseFloat(
-    calDistance(originCoordinates, [dog?.lat, dog?.lng]),
-  ).toFixed(1);
-
   return (
     <Link to={`/findplaydates/dogs/${dog.id}?lat=${dog.lat}&lng=${dog.lng}`}>
       <ul className="grid w-full grid-cols-profile grid-rows-3 rounded-lg border-1 border-gray-300 p-2 text-xs hover:border-org">
@@ -53,7 +18,7 @@ const MiniProfile = ({ dog }) => {
         <li>{dog?.sex}</li>
         <li>{calculateAge(dog?.dateOfBirth)}</li>
         <li>{dog?.size} lb</li>
-        <li>{distance} km</li>
+        <li>{dog?.distance} km</li>
       </ul>
     </Link>
   );
