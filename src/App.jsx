108fee83b505b283751960c252504914942700db
Kelly Lee <kellyws.lee@gmail.com>
2024-01-10 17:49:55 -0500

    perf: Add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-// import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppLayout from './ui/AppLayout';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import PrivateChats from './features/privateChats/PrivateChats';
-import OpenChats from './features/openChats/OpenChats';
-import Playdates from './features/playdates/Playdates';
-import Profile from './features/profile/Profile';
 import FindPlaydates from './pages/FindPlaydates';
 import DogList from './features/findPlaydates/DogList';
 import FullProfile from './features/findPlaydates/FullProfile';
-import DevContact from './pages/DevContact';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
 import ProtectedRoute from './ui/ProtectedRoute';
 import PageNotFound from './pages/PageNotFound';
 import CustomToaster from './ui/Toaster';
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const PrivateChats = lazy(() => import('./features/privateChats/PrivateChats'));
+const OpenChats = lazy(() => import('./features/openChats/OpenChats'));
+const Playdates = lazy(() => import('./features/playdates/Playdates'));
+const Profile = lazy(() => import('./features/profile/Profile'));
+const DevContact = lazy(() => import('./pages/DevContact'));
+const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/SignUp'));
 
 const App = () => {
   const queryClient = new QueryClient({
@@ -36,66 +37,68 @@ const App = () => {
       </div>
 
       <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Home />} />
-            <Route path="contact" element={<DevContact />} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Home />} />
+              <Route path="contact" element={<DevContact />} />
 
-            <Route path="findplaydates" element={<FindPlaydates />}>
-              <Route index element={<Navigate replace to="dogs" />} />
-              <Route path="dogs" element={<DogList />} />
-              <Route path="dogs/:id" element={<FullProfile />} />
+              <Route path="findplaydates" element={<FindPlaydates />}>
+                <Route index element={<Navigate replace to="dogs" />} />
+                <Route path="dogs" element={<DogList />} />
+                <Route path="dogs/:id" element={<FullProfile />} />
+              </Route>
+
+              <Route
+                path="dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="myplaydates" />} />
+                <Route
+                  path="myplaydates"
+                  element={
+                    <ProtectedRoute>
+                      <Playdates />
+                    </ProtectedRoute>
+                  }
+                />
+
+                <Route
+                  path="profile"
+                  element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="openchats"
+                  element={
+                    <ProtectedRoute>
+                      <OpenChats />
+                    </ProtectedRoute>
+                  }
+                />
+
+                <Route
+                  path="privatechats"
+                  element={
+                    <ProtectedRoute>
+                      <PrivateChats />
+                    </ProtectedRoute>
+                  }
+                />
+              </Route>
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="*" element={<PageNotFound />} />
             </Route>
-
-            <Route
-              path="dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="myplaydates" />} />
-              <Route
-                path="myplaydates"
-                element={
-                  <ProtectedRoute>
-                    <Playdates />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                path="profile"
-                element={
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="openchats"
-                element={
-                  <ProtectedRoute>
-                    <OpenChats />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                path="privatechats"
-                element={
-                  <ProtectedRoute>
-                    <PrivateChats />
-                  </ProtectedRoute>
-                }
-              />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <CustomToaster />
     </QueryClientProvider>
